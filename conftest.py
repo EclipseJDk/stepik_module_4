0d3dc5f9f9f15df758da0cc610d6fd237d764d38
eclipse <eclipse0044@gmail.com>
2021-11-03 14:32:52 +0200

    ver_01 upload files

---
+++ conftest.py
@@ -0,0 +1,31 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser', action='store', default='chrome',
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose lang")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser")
+    user_language = request.config.getoption("language")
+    if browser_name == "chrome":
+        print("\nStart browser chrome for test...")
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        print("\nStart browser firefox for test...")
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)
+        browser = webdriver.Firefox(firefox_profile=fp)
+    else:
+        print(f"Error! You need to choose Chrome or FireFox. Unexpected option: {browser_name}")
+    yield browser
+    print("\nquit browser...")
+    browser.quit()
